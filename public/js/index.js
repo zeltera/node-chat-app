ae978b594ec675fe639e4277c2830f1da6d2c931
Adrian Zelter <zeltera@yahoo.com>
2018-12-23 21:51:37 +0200

    add scroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,23 @@
 let socket = io();
+
+function scrollToBottom(){
+	//selectors
+	let messages = $('#messages')
+	let newMessage = messages.children("li:last-child");
+
+
+	//heights
+	let clientHeight = messages.prop("clientHeight");
+	let scrollTop = messages.prop("scrollTop");
+	let scrollHeight = messages.prop("scrollHeight");
+	let newMessageHeight = newMessage.innerHeight();
+	let lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+};
+
 socket.on('connect', () => {
 	console.log("connected to server");
 
@@ -24,6 +43,7 @@ socket.on('newMessage', (message) => {
 		createdAt: formatedTime
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
@@ -36,6 +56,7 @@ socket.on('newLocationMessage', (message) => {
 		createdAt: formatedTime
 	});
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 
